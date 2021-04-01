af9ee64d9c8b204cc6f010a9fcc7b458829bbe6b
unknown <ibmjavanh16@iiht.tech>
2021-04-01 08:46:02 +0530

    changes

--- demo/demo/src/main/java/com/ibm/demo/SpringBootdemo1Application.java
+++ demo/demo/src/main/java/com/ibm/demo/SpringBootdemo1Application.java
@@ -1,19 +1,25 @@
 package com.ibm.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringBootdemo1Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootdemo1Application.class, args);
 	}
+
 	@Bean
 	RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
-
+	@Bean
+	PlatformTransactionManager transactionManager(){
+		return new MongoTransactionManager();
+	}
 }
--- demo/demo/src/main/java/com/ibm/demo/service/OrderService.java
+++ demo/demo/src/main/java/com/ibm/demo/service/OrderService.java
@@ -6,37 +6,47 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import com.ibm.demo.repo.OrderRepository;
 import com.ibm.demo.entity.Order;
 
 @Service
-public class OrderService {//spring beans
-	@Autowired
+public class OrderService {// spring beans classes with special annotation
+	@Autowired //dependency of repository class
 	OrderRepository orderRepository;
 	@Autowired
 	RestTemplate getTaxesTemplate;
-public String createOrder(Order order) {
-	//call getTaxes
-	Float tax=getTaxesTemplate.getForObject("http://localhost:8080/getTaxes?price={price}",Float.class,order.getPrice() );
-	System.out.println(tax);
-	order.setTax(tax);
-	Order savedOrder= orderRepository.save(order);
-	return savedOrder.getId();
-}
+    @Transactional
+	public String createOrder(Order order) {
+		// call getTaxes
+		//Float tax = getTaxesTemplate.getForObject("http://localhost:8080/getTaxes?price={price}", Float.class,
+				//order.getPrice());
+		//System.out.println(tax);
+		//order.setTax(tax);
+		Order savedOrder = orderRepository.save(order);
+		//if(order!=null)
+		//throw new RuntimeException();
+		return savedOrder.getId();
+	}
 
+	public void updateOrder(Order order) {
+		orderRepository.save(order);
 
+	}
 
-public void updateOrder(Order order) {
-	orderRepository.save(order);
+	public void deleteOrder(String orderId) {
+          orderRepository.deleteById(orderId);
+	}
+
 	
-}
+	public OrderRepository getOrderRepository() {
+		return orderRepository;
+	}
 
-public void deleteOrder(String orderId) {
-	
-	orderRepository.deleteById(orderId);
-}
-
+	public void setOrderRepository(OrderRepository orderRepository) {
+		this.orderRepository = orderRepository;
+	}
 
 }
